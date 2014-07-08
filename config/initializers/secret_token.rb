5ff81f863c3955630a0c22e2bf3f301b17b974fa
yuki3738 <y.minamiya@gmail.com>
2014-07-09 02:28:41 +0900

    20140709 次はP148 _sidebar.html.erbから

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asagao::Application.config.secret_token = '75b82f8728c9fa5c5fd4fb327f23ec8e239e6a2f154da4b481941b9f753a2ccdd653a115f9ceb6a517527e89bbfd39040dd64ba1a7861bb29f568a4f4147c9a5'
