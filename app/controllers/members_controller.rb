0285d372d66cf4109d03b4d3e59173b3b06f6c91
yuki3738 <y.minamiya@gmail.com>
2014-07-13 20:08:57 +0900

    次はP232から

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -21,9 +21,22 @@ class MembersController < ApplicationController
   end
 
   def create
+    @member = Member.new(params[:member])
+    if @member.save
+      redirect_to @member, notice: "会員を登録しました。"
+    else
+      render "new"
+    end
   end
 
   def update
+    @member = Member.find(params[:id])
+    @member.assign_attributes(params[:member])
+    if @member.save
+      redirect_to @member, notice: "会員情報を更新しました。"
+    else
+      render "edit"
+    end
   end
 
   def destroy
