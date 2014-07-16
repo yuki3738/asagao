912eb16a64162e30d8e488579a7d837432e44dc8
yuki3738 <y.minamiya@gmail.com>
2014-07-16 22:38:11 +0900

    6.3が終了

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,17 +1,18 @@
 class Member < ActiveRecord::Base
+  include EmailAddressChecker
   attr_accessible :number, :name, :full_name, :email, :birthday, :gender, :administrator
-
+  
   validates :number, presence: true,
     numericality: { only_integer: true,
-      greater_than: 0, less_than: 100, allow_blank: true },
+       greater_than: 0, less_than: 100, allow_blank: true },
     uniqueness: true
-  validates :number, presence: true,
+  validates :name, presence: true,
     format: { with: /\A[A-Za-z]\w*\z/, allow_blank: true },
-    length: { minimum, 2, maximum: 20, allow_blank: true },
+    length: { minimum: 2, maximum: 20, allow_blank: true },
     uniqueness: { case_sensitive: false }
-
   validates :full_name, length: { maximum: 20 }
-  
+  validate :check_email
+
   class << self
     def search(query)
       rel = order("number")
@@ -20,6 +21,14 @@ class Member < ActiveRecord::Base
                 "%#{query}%", "%#{query}%")
       end
       rel
+    end
+  end
+
+  private
+  def check_email
+    if email.present?
+      errors.add(:email, :invalid) unless
+          well_formed_as_email_address(email)
     end
   end
 end
