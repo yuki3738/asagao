e8fde6d10afbaf898c26c11d21048cb81c76197b
yuki3738 <y.minamiya@gmail.com>
2014-07-20 09:38:07 +0900

    7.2終了

--- test/unit/member_test.rb
+++ test/unit/member_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
   test "the truth" do
-   assert true
+    assert true
+  end
+
+  test "factory girl" do
+    member = Factory(:member)
+    assert_equal "Yamada Taro", member.full_name
   end
 end
