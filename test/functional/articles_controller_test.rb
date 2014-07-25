d1396a35f58ffb20ae775827156ad55f5c23e62d
yuki3738 <y.minamiya@gmail.com>
2014-07-26 00:53:18 +0900

    次はP300 TopControllerの修正から

--- test/functional/articles_controller_test.rb
+++ test/functional/articles_controller_test.rb
@@ -24,4 +24,39 @@ class ArticlesControllerTest < ActionController::TestCase
     get :edit, id: article
     assert_response :success
   end
+
+  test "create" do
+    post :create, article: Factory.attributes_for(:article)
+    article = assigns[:article]
+    assert_redirected_to article
+  end
+
+  test "update" do
+    article = Factory(:article)
+    put :update, id: article, article: Factory.attributes_for(:article)
+    assert_redirected_to article
+  end
+
+  test "fail to create" do
+    attrs = Factory.attributes_for(:article, title: "")
+    post :create, article: attrs
+    assert_response :success
+    assert_template "new"
+  end
+
+  test "fail to update" do
+    attrs = Factory.attributes_for(:article, body: "")
+    article = Factory(:article)
+    put :update, id: article, article: attrs
+    assert_response :success
+    assert_template "edit"
+  end
+
+  test "destroy" do
+    article = Factory(:article)
+    delete :destroy, id: article
+    assert_redirected_to :articles
+    assert_raise(ActiveRecord::RecordNotFound) {
+      Article.find(article.id) }
+  end
 end
