848f222d4f9533ed88bdeff6eb5fe93bfcb69e3d
yuki3738 <y.minamiya@gmail.com>
2014-07-26 18:04:03 +0900

    Chapter7が終了

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
   def index
+    @articles = Article.readable.order("released_at DESC").limit(5)
   end
 end
--- test/functional/top_controller_test.rb
+++ test/functional/top_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class TopControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "index" do
+    2.times { Factory(:article) }
+    Factory(:article, released_at: 1.hours.from_now)
     get :index
     assert_response :success
+    assert_select "div#content h2", 2
   end
 
 end
