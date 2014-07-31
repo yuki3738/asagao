e9d09cecb93562c31ef10be0c7940d52b6477f25
yuki3738 <y.minamiya@gmail.com>
2014-07-31 22:43:42 +0900

    次はP352 管理ページのテスト

---
+++ app/controllers/admin/articles_controller.rb
@@ -0,0 +1,42 @@
+class Admin::ArticlesController < Admin::Base
+  def index
+    @articles = Article.order("released_at DESC")
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    if @article.save
+      redirect_to [:admin, @article], notice: "ニュース記事を登録しました。"
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.assign_attributes(params[:article])
+    if @article.save
+      redirect_to @article, notice: "ニュース記事を更新しました。"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to :admin_articles, notice: "ニュース記事を削除しました。"
+  end
+end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,42 +1,9 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.order("released_at DESC")
+    @articles = Article.readable_for(@current_member).order("released_at DESC")
   end
 
   def show
-    @article = Article.find(params[:id])
-  end
-
-  def new
-    @article = Article.new
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def create
-    @article = Article.new(params[:article])
-    if @article.save
-      redirect_to @article, notice: "ニュース記事を登録しました。"
-    else
-      render "new"
-    end
-  end
-
-  def update
-    @article = Article.find(params[:id])
-    @article.assign_attributes(params[:article])
-    if @article.save
-      redirect_to @article, notice: "ニュース記事を更新しました。"
-    else
-      render "edit"
-    end
-  end
-
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    redirect_to :articles, notice: "ニュース記事を削除しました。"
+    @article = Article.readable_for(@current_member).find(params[:id])
   end
 end
